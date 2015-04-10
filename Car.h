dfadc8a3ba1a5b5f39c30dbef41fe1f4f1dd6092
Yung Dai <yungchidai@gmail.com>
2015-04-10 10:21:05 -0400

    Naming Conventions

---
+++ Car.h
@@ -0,0 +1,51 @@
+//
+//  Car.h
+//  Assignment6
+//
+//  Created by Yung Dai on 2015-04-10.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+// Accessors
+- (BOOL)isRunning;
+- (void)setRunning:(BOOL)running;
+- (NSString *)model;
+- (void)setModel:(NSString *)model;
+
+// Calculated values
+- (double)maximumSpeed;
+- (double)maximumSpeedUsingLocale:(NSLocale *)locale;
+
+// Action methods
+- (void)startEngine;
+- (void)driveForDistance:(double)theDistance;
+- (void)driveFromOrigin:(id)theOrigin toDestination:(id)theDestination;
+- (void)turnByAngle:(double)theAngle;
+- (void)turnToAngle:(double)theAngle;
+
+// Error handling methods
+- (BOOL)loadPassenger:(id)aPassenger error:(NSError **)error;
+
+// Constructor methods
+- (id)initWithModel:(NSString *)aModel;
+- (id)initWithModel:(NSString *)aModel mileage:(double)theMileage;
+
+// Comparison methods
+- (BOOL)isEqualToCar:(Car *)anotherCar;
+- (Car *)fasterCar:(Car *)anotherCar;
+- (Car *)slowerCar:(Car *)anotherCar;
+
+// Factory methods
++ (Car *)car;
++ (Car *)carWithModel:(NSString *)aModel;
++ (Car *)carWithModel:(NSString *)aModel mileage:(double)theMileage;
+
+// Singleton methods
++ (Car *)sharedCar;
+
+@end
+
---
+++ Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  Assignment6
+//
+//  Created by Yung Dai on 2015-04-10.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
